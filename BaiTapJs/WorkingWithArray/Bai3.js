a3eeff1bfdf404b6ea4c64579c97cb865f514de6
manhhieu1204 <vomanhhieu62@gmail.com>
2024-08-28 22:11:11 +0700

    Buoi 2 Js: Bai 3 WorkingWithArray

---
+++ BaiTapJs/WorkingWithArray/Bai3.js
@@ -0,0 +1,15 @@
+const calcAverageHumanAge = (ages) => {
+	let newAges = ages
+		.map((age) => {
+			if (age > 2) {
+				return 16 + age * 4;
+			} else {
+				return 2 * age;
+			}
+		})
+		.filter((age) => age >= 18);
+	return newAges.reduce((sum, age) => sum + age, 0) / newAges.length;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]).toFixed(2));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]).toFixed(2));
